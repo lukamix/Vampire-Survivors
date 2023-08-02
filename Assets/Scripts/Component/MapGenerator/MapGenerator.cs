319968874bc4f84086894a386d7b04729b42db01
linhnguyenthi20 <62656512+lukamix@users.noreply.github.com>
2023-08-02 16:37:48 +0700

    add result

--- Assets/Scripts/Component/MapGenerator/MapGenerator.cs
+++ Assets/Scripts/Component/MapGenerator/MapGenerator.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 using UnityEditor;
 using System;
 using System.Linq;
-using static UnityEditor.Experimental.AssetDatabaseExperimental.AssetDatabaseCounters;
 
 public class MapGenerator : MonoBehaviour
 {
